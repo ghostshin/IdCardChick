5a10c6298b00ad37e1bbe3bccf00a2a5f2555426
xushen <xlelouchshin@163.com>
2020-06-30 22:07:31 +0800

    身份证校验器

---
+++ src/idcard.java
@@ -0,0 +1,11 @@
+
+public class idcard {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		IdcardValidator dcardValidator = new IdcardValidator();
+		
+		System.out.println(dcardValidator.isValidatedAllIdcard("110101199001014104"));
+	}
+
+}
